9f6eaa0924d206d44c7ba2610d2e5e980ae41386
Renan Epifânio <renan.epifanio@outlook.com>
2022-05-01 23:08:52 -0300

    several files update

--- src/modules/setData.ts
+++ src/modules/setData.ts
@@ -96,7 +96,7 @@ export default class SetProductData {
 
   init(): this {
     const data: Promise<T.Data[] | undefined> = getProductData(
-      "../../product-data.json"
+      "../../Ecommerce-Product/product-data.json"
     );
     this.setImgs(data);
     this.setInfos(data);
